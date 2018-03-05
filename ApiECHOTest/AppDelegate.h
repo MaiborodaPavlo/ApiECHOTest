43a15ba9503f1e4c5323cfec0e7e5d8ea7c2c481
Pavel Mayboroda <Successbloger1@gmail.com>
2018-03-05 16:16:43 +0200

    Initial Commit

---
+++ ApiECHOTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ApiECHOTest
+//
+//  Created by Pavel on 05.03.2018.
+//  Copyright © 2018 Pavel Maiboroda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ApiECHOTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ApiECHOTest
+//
+//  Created by Pavel on 05.03.2018.
+//  Copyright © 2018 Pavel Maiboroda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
